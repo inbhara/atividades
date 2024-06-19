bc40af23c99097692b9efe09a7c2074ab743dca2
inbhara <isa.namie07@gmail.com>
2024-06-19 15:40:44 -0300

    Create TelaLogin.java

---
+++ TelaLogin/src/telalogin/TelaLogin.java
@@ -0,0 +1,13 @@
+
+package telalogin;
+
+import interfaces.Login;
+
+public class TelaLogin {
+
+    public static void main(String[] args) {
+        Login tl = new Login();
+        tl.setVisible(true);
+    }
+    
+}
